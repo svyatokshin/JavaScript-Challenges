dd587e25e00ac27167de7d28f773fcf4c719d64d
svyatokshin <svyatokshin@gmail.com>
2021-06-17 19:16:16 -0400

    added another problem

--- JavaScript-Problems.js
+++ JavaScript-Problems.js
@@ -74,3 +74,63 @@ const removeElement = (nums, val) => {
     }
       return nums.length
   }
+
+
+
+
+// PROBLEM 3
+
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+// It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+// Example 1:
+// Input: candidates = [2,3,6,7], target = 7
+// Output: [[2,2,3],[7]]
+// Explanation:
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 7 is a candidate, and 7 = 7.
+// These are the only two combinations.
+
+// Example 2:
+// Input: candidates = [2,3,5], target = 8
+// Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+// Example 3:
+// Input: candidates = [2], target = 1
+// Output: []
+
+// Example 4:
+// Input: candidates = [1], target = 1
+// Output: [[1]]
+
+// Example 5:
+// Input: candidates = [1], target = 2
+// Output: [[1,1]]
+
+var combinationSum = function(candidates, target) {
+    let combinations = [];
+    let n = candidates.length - 1;
+    
+    // since we subtract the target in each recursive call, we know that the base case is when target === 0
+    function findCombinations(nums, target, sIndex) {
+      if (target === 0) {combinations.push([...nums])}
+      for (let i = sIndex; i <= n; i++) {
+        // check if the candidate is less than target
+        const c = candidates[i];
+        if (c <= target) {
+          // do
+          nums.push(c);
+          
+          // recur
+          findCombinations(nums, target - c, i);
+          
+          // undo
+          nums.pop();
+        }
+      }
+    }
+    
+    findCombinations([],target, 0);
+    return combinations;
+  };
