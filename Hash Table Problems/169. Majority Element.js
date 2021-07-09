a6255f3ae4942dd4a0a5624831b695c4d4ddaaf2
svyatokshin <svyatokshin@gmail.com>
2021-07-09 18:44:47 -0400

    reorganized and added an addition solution to hash table problem

--- Hash Table Problems/169. Majority Element.js
+++ Hash Table Problems/169. Majority Element.js
@@ -55,3 +55,15 @@
         }
     }
 };
+
+
+// Shorter solution: 
+var majorityElement = function(nums) {
+    var obj = {};
+    
+    for(var i = 0; i < nums.length; i++){
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+        if(obj[nums[i]] > nums.length / 2)  return nums[i];
+    }
+    
+};
