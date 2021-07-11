ed601115a1c5a530e730e1a567fdeaa3088d784c
svyatokshin <svyatokshin@gmail.com>
2021-07-11 14:07:51 -0400

    solved interview problem

--- FuzzyFind (Problem I had for Bloomberg Interview).js
+++ FuzzyFind (Problem I had for Bloomberg Interview).js
@@ -21,3 +21,39 @@
 // fuzzyfind(s, f) # => []
 
 
+const s1 = "cbazer";
+const s2 = "ace";
+const s3 = "123";
+const f = [
+  "fff/eee",
+  "ace",
+  "alpha/core",
+  "ac1",
+  "asdfc",
+  "cbazer",
+  "asdcbfdebfrdaewrzdfgr",
+  "asdcbfdebfrdaewrzdfegr",
+  "145684580682504876408576403",
+  "aaodsufhoasudhfoaudhsfouadoau123",
+  "123043585708237081702123102841023710823123123",
+  "321321321321321321321321321",
+];
+const FuzzyFind = (s, f) => {
+  let sp = 0;
+  const arr = [];
+  for (let i = 0; i < f.length; i++) {
+    for (let j = 0; j < f[i].length; j++) {
+      if (f[i][j] == s[sp]) {
+        sp++;
+      }
+    }
+    if (sp > s.length - 1) {
+      arr.push(f[i]);
+    }
+    sp = 0;
+  }
+  return arr;
+};
+console.log(FuzzyFind(s1, f));
+console.log(FuzzyFind(s2, f));
+console.log(FuzzyFind(s3, f));
