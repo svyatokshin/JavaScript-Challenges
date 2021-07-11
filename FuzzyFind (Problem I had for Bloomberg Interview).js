8dfc435f0692e3b131a1691d4354b6cd3e102ebf
svyatokshin <svyatokshin@gmail.com>
2021-07-11 14:07:18 -0400

    added problem from interview a few weeks ago

---
+++ FuzzyFind (Problem I had for Bloomberg Interview).js
@@ -0,0 +1,23 @@
+// Task Description
+// Objective
+// Define a method fuzzyFind that takes a search term string S and a list of filename strings F to search through.
+// The method search return another list R that is the results of Fuzzy Searching through F with S. The returned list 
+// R contains the filename strings from F that Fuzzy Matched with S.
+// To Fuzzy Search through a single string T the characters in S should be present in T in the same order but, with
+// any other characters in between.Define
+
+// Examples:
+
+// S = "ac"
+// F = ['alpha/core']
+// fuzzyfind(s, f) # => ['alpha/core']
+
+// S = "acx"
+// F = ['alpha/core']
+// fuzzyfind(s, f) # => []
+
+// S = "acp"
+// F = ['alpha/core']
+// fuzzyfind(s, f) # => []
+
+
