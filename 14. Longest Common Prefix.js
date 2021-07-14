1759aa11de590f28359574688899c063923c8b31
svyatokshin <svyatokshin@gmail.com>
2021-07-13 22:27:42 -0400

    solved

--- 14. Longest Common Prefix.js
+++ 14. Longest Common Prefix.js
@@ -30,3 +30,26 @@
 // 1 <= strs.length <= 200
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lower-case English letters.
+
+
+
+var longestCommonPrefix = function(strs) {
+     if (!strs.length) return "";
+
+     let longest = "";
+     let firstWord = strs[0];
+
+     for (let i = 0; i < firstWord.length; i++) {
+         let char = firstWord[i];
+
+         for (let j = 1; j < strs.length; j++) {
+             let otherWord = strs[j]
+             let otherChar = otherWord[i];
+             if (char !== otherChar || !otherChar) return longest;
+         }
+
+         longest += char;
+     }
+
+     return longest;
+};
